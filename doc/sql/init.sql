d31a9c51d3c70b6cb9dd68c430f64c224692a7a9
bowenWan12 <769145832@qq.com>
2020-08-21 16:35:53 +0800

    init oauth sql

---
+++ doc/sql/init.sql
@@ -0,0 +1,129 @@
+DROP TABLE IF EXISTS `sys_user`;
+CREATE TABLE `sys_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+  `login_name` varchar(36) DEFAULT NULL COMMENT '登录名',
+  `nick_name` varchar(40) DEFAULT NULL COMMENT '昵称',
+  `icon` varchar(2000) DEFAULT NULL,
+  `password` varchar(40) DEFAULT NULL COMMENT '密码',
+  `salt` varchar(40) DEFAULT NULL COMMENT 'shiro加密盐',
+  `tel` varchar(11) DEFAULT NULL COMMENT '手机号码',
+  `email` varchar(200) DEFAULT NULL COMMENT '邮箱地址',
+  `locked` tinyint(2) DEFAULT NULL COMMENT '是否锁定',
+  `create_date` datetime DEFAULT NULL,
+  `create_by` bigint(20) DEFAULT NULL,
+  `update_date` datetime DEFAULT NULL,
+  `update_by` bigint(20) DEFAULT NULL,
+  `remarks` varchar(255) DEFAULT NULL,
+  `del_flag` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `sys_role`;
+CREATE TABLE `sys_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) DEFAULT NULL COMMENT '角色名称',
+  `create_date` datetime DEFAULT NULL,
+  `create_by` bigint(20) DEFAULT NULL,
+  `update_date` datetime DEFAULT NULL,
+  `update_by` bigint(20) DEFAULT NULL,
+  `remarks` varchar(255) DEFAULT NULL,
+  `del_flag` tinyint(2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `sys_menu`;
+CREATE TABLE `sys_menu` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) DEFAULT NULL COMMENT '菜单名称',
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '父菜单',
+  `level` bigint(2) DEFAULT NULL COMMENT '菜单层级',
+  `parent_ids` varchar(2000) DEFAULT NULL COMMENT '父菜单联集',
+  `sort` smallint(6) DEFAULT NULL COMMENT '排序',
+  `href` varchar(2000) DEFAULT NULL COMMENT '链接地址',
+  `target` varchar(20) DEFAULT NULL COMMENT '打开方式',
+  `icon` varchar(100) DEFAULT NULL COMMENT '菜单图标',
+  `bg_color` varchar(255) DEFAULT NULL COMMENT '显示背景色',
+  `is_show` tinyint(2) DEFAULT NULL COMMENT '是否显示',
+  `permission` varchar(200) DEFAULT NULL COMMENT '权限标识',
+  `create_by` bigint(20) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `update_by` bigint(20) DEFAULT NULL,
+  `update_date` datetime DEFAULT NULL,
+  `remarks` varchar(255) DEFAULT NULL,
+  `del_flag` tinyint(2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `sys_user_role`;
+CREATE TABLE `sys_user_role` (
+  `user_id` bigint(20) NOT NULL,
+  `role_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `sys_role_menu`;
+CREATE TABLE `sys_role_menu` (
+  `role_id` bigint(20) NOT NULL,
+  `menu_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`role_id`,`menu_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `sys_group`;
+CREATE TABLE `sys_group` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) DEFAULT NULL COMMENT '分组名称',
+  `parent_id` bigint(20) DEFAULT NULL COMMENT '父分组ID',
+  `level` bigint(2) DEFAULT NULL,
+  `parent_ids` varchar(2000) DEFAULT NULL COMMENT '分组序列号',
+  `sort` smallint(6) DEFAULT NULL COMMENT '分组排序值',
+  `create_by` bigint(20) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `update_by` bigint(20) DEFAULT NULL,
+  `update_date` datetime DEFAULT NULL,
+  `remarks` varchar(255) DEFAULT NULL,
+  `del_flag` tinyint(2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `sys_group_ur`;
+CREATE TABLE `sys_group_ur` (
+  `group_id` bigint(20) NOT NULL COMMENT '分组ID',
+  `user_id` bigint(20) DEFAULT NULL COMMENT '用户ID',
+  `role_id` bigint(20) DEFAULT NULL COMMENT '角色ID',
+  PRIMARY KEY (`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `sys_log`;
+CREATE TABLE `sys_log` (
+  `id` bigint(50) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `type` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT '请求类型',
+  `title` varchar(255) COLLATE utf8_bin DEFAULT '' COMMENT '日志标题',
+  `remote_addr` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '操作IP地址',
+  `username` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '操作用户昵称',
+  `request_uri` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '请求URI',
+  `http_method` varchar(10) COLLATE utf8_bin DEFAULT NULL COMMENT '操作方式',
+  `class_method` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '请求类型.方法',
+  `params` text COLLATE utf8_bin COMMENT '操作提交的数据',
+  `session_id` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT 'sessionId',
+  `response` longtext COLLATE utf8_bin COMMENT '返回内容',
+  `use_time` bigint(11) DEFAULT NULL COMMENT '方法执行时间',
+  `browser` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '浏览器信息',
+  `area` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '地区',
+  `province` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '省',
+  `city` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '市',
+  `isp` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '网络服务提供商',
+  `exception` text COLLATE utf8_bin COMMENT '异常信息',
+  `create_by` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT '创建者',
+  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` bigint(64) DEFAULT NULL,
+  `update_date` datetime DEFAULT NULL,
+  `remarks` varchar(255) COLLATE utf8_bin DEFAULT NULL,
+  `del_flag` bit(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `sys_log_create_by` (`create_by`) USING BTREE,
+  KEY `sys_log_request_uri` (`request_uri`) USING BTREE,
+  KEY `sys_log_type` (`type`) USING BTREE,
+  KEY `sys_log_create_date` (`create_date`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=223 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='系统日志';
+INSERT INTO `sys_user` VALUES ('1', 'test', '我是管理员', 'https://github.com/bowenWan12/blog-bm/blob/master/src/assets/image/user.png', '810339f5426fe2dcaf92c5cac718acc6471a034b', '3fb62b5aeede1bbf', '13776055179', '769145832@qq.com', '0', '2020-08-21 12:19:39', '1', '2020-08-21 16:27:15', '1', '', '0');
+INSERT INTO `sys_role` VALUES ('1', '超级管理员', '2017-11-02 14:19:07', '1', '2018-02-08 13:24:35', '1', '', '0');
+INSERT INTO `sys_user_role` VALUES ('1', '1');
