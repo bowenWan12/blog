2e3983ba76f0fee7f35e6c33e8b48708ab34805d
bowenWan12 <769145832@qq.com>
2020-08-27 17:48:05 +0800

    login func

--- doc/sql/init.sql
+++ doc/sql/init.sql
@@ -34,13 +34,14 @@ CREATE TABLE `sys_role` (
 DROP TABLE IF EXISTS `sys_menu`;
 CREATE TABLE `sys_menu` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(40) DEFAULT NULL COMMENT '菜单名称',
+  `name` varchar(20) DEFAULT NULL COMMENT '菜单名称',
+	`desc` varchar(40) DEFAULT NULL COMMENT '菜单描述',
   `parent_id` bigint(20) DEFAULT NULL COMMENT '父菜单',
   `level` bigint(2) DEFAULT NULL COMMENT '菜单层级',
   `parent_ids` varchar(2000) DEFAULT NULL COMMENT '父菜单联集',
   `sort` smallint(6) DEFAULT NULL COMMENT '排序',
-  `href` varchar(2000) DEFAULT NULL COMMENT '链接地址',
-  `target` varchar(20) DEFAULT NULL COMMENT '打开方式',
+  `href` varchar(2000) DEFAULT NULL COMMENT '链接path',
+  `target` varchar(20) DEFAULT NULL COMMENT 'url地址',
   `icon` varchar(100) DEFAULT NULL COMMENT '菜单图标',
   `bg_color` varchar(255) DEFAULT NULL COMMENT '显示背景色',
   `is_show` tinyint(2) DEFAULT NULL COMMENT '是否显示',
@@ -53,6 +54,12 @@ CREATE TABLE `sys_menu` (
   `del_flag` tinyint(2) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=63 DEFAULT CHARSET=utf8;
+INSERT INTO `sys_menu` VALUES ('1', 'home', '首页', '', '1', '1,', '1', '/', 'Home/Home', 's-home', '', '1', 'sys_home', '1', '2018-01-16 11:29:46', '1', '2018-01-20 03:09:26', null, '0');
+INSERT INTO `sys_menu` VALUES ('2', 'user', '系统用户管理', '', '1', '2,', '2', '/user', 'UserManage/UserManage', 'user', '', '1', 'sys:user:list', '1', '2018-01-16 11:31:18', '1', '2018-01-20 05:59:20', null, '0');
+INSERT INTO `sys_menu` VALUES ('3', '', '其他', '', '1', '3,', '3', '/', '', 'location', '', '1', 'sys_other_list', '1', '2018-01-16 11:31:18', '1', '2018-01-20 05:59:20', null, '0');
+INSERT INTO `sys_menu` VALUES ('4', 'page1', '页面1', '3', '2', '3,4,', '4', '/page1', 'Other/PageOne', 'setting', '', '1', 'sys_other_list', '1', '2018-01-16 11:31:18', '1', '2018-01-20 05:59:20', null, '0');
+INSERT INTO `sys_menu` VALUES ('5', 'page2', '页面2', '3', '2', '3,5,', '5', '/page2', 'Other/PageTwo', 'setting', '', '1', 'sys_other_list', '1', '2018-01-16 11:31:18', '1', '2018-01-20 05:59:20', null, '0');
+
 
 DROP TABLE IF EXISTS `sys_user_role`;
 CREATE TABLE `sys_user_role` (
@@ -124,6 +131,48 @@ CREATE TABLE `sys_log` (
   KEY `sys_log_type` (`type`) USING BTREE,
   KEY `sys_log_create_date` (`create_date`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=223 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='系统日志';
+DROP TABLE IF EXISTS `sys_rescource`;
+CREATE TABLE `sys_rescource` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `file_name` varchar(255) DEFAULT NULL COMMENT '文件名称',
+  `source` varchar(255) DEFAULT NULL COMMENT '来源',
+  `web_url` varchar(500) DEFAULT NULL COMMENT '资源网络地址',
+  `hash` varchar(255) DEFAULT NULL COMMENT '文件标识',
+  `file_size` varchar(50) DEFAULT NULL COMMENT '文件大小',
+  `file_type` varchar(255) DEFAULT NULL COMMENT '文件类型',
+  `original_net_url` text,
+  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `create_by` bigint(20) DEFAULT NULL COMMENT '创建人',
+  `update_date` datetime DEFAULT NULL COMMENT '修改时间',
+  `update_by` bigint(20) DEFAULT NULL COMMENT '修改人',
+  `remarks` varchar(255) DEFAULT NULL COMMENT '备注',
+  `del_flag` tinyint(4) DEFAULT NULL COMMENT '删除标记',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='系统资源';
+
+DROP TABLE IF EXISTS `sys_dict`;
+CREATE TABLE `sys_dict` (
+  `id` bigint(64) NOT NULL AUTO_INCREMENT COMMENT '编号',
+  `value` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '数据值',
+  `label` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '标签名',
+  `type` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '类型',
+  `description` varchar(100) COLLATE utf8_bin DEFAULT NULL COMMENT '描述',
+  `sort` int(10) DEFAULT NULL COMMENT '排序（升序）',
+  `parent_id` varchar(64) COLLATE utf8_bin DEFAULT '0' COMMENT '父级编号',
+  `create_by` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT '创建者',
+  `create_date` datetime DEFAULT NULL COMMENT '创建时间',
+  `update_by` varchar(64) COLLATE utf8_bin DEFAULT NULL COMMENT '更新者',
+  `update_date` datetime DEFAULT NULL COMMENT '更新时间',
+  `remarks` varchar(255) COLLATE utf8_bin DEFAULT NULL COMMENT '备注信息',
+  `del_flag` char(1) COLLATE utf8_bin NOT NULL DEFAULT '0' COMMENT '删除标记',
+  PRIMARY KEY (`id`),
+  KEY `sys_dict_value` (`value`) USING BTREE,
+  KEY `sys_dict_label` (`label`) USING BTREE,
+  KEY `sys_dict_del_flag` (`del_flag`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=125 DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='字典表,测试表';
+
+
+
 INSERT INTO `sys_user` VALUES ('1', 'test', '我是管理员', 'https://github.com/bowenWan12/blog-bm/blob/master/src/assets/image/user.png', '810339f5426fe2dcaf92c5cac718acc6471a034b', '3fb62b5aeede1bbf', '13776055179', '769145832@qq.com', '0', '2020-08-21 12:19:39', '1', '2020-08-21 16:27:15', '1', '', '0');
 INSERT INTO `sys_role` VALUES ('1', '超级管理员', '2017-11-02 14:19:07', '1', '2018-02-08 13:24:35', '1', '', '0');
 INSERT INTO `sys_user_role` VALUES ('1', '1');
