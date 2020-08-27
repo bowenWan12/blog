21145b8bc97f703f21b1a4614dd39e0f3e120e7d
bowenWan12 <769145832@qq.com>
2020-08-27 09:24:51 +0800

    跨域配置（忘提交）

---
+++ blog-bm/src/main/java/com/winds/bm/dto/LoginUser.java
@@ -0,0 +1,43 @@
+package com.winds.bm.dto;
+
+import java.io.Serializable;
+
+public class LoginUser implements Serializable {
+
+    private String username;
+    private	String password;
+    private	String rememberMe;
+    private	String verifycode;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRememberMe() {
+        return rememberMe;
+    }
+
+    public void setRememberMe(String rememberMe) {
+        this.rememberMe = rememberMe;
+    }
+
+    public String getVerifycode() {
+        return verifycode;
+    }
+
+    public void setVerifycode(String verifycode) {
+        this.verifycode = verifycode;
+    }
+}
